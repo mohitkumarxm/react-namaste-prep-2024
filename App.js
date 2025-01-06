643554285bb6106d54b4fb306cc62517e84b7c31
Mohit Kumar <mohitkumarxm@gmail.com>
2025-01-06 15:39:50 +0530

    new start commit for food app project

--- App.js
+++ App.js
@@ -1,9 +1,2 @@
-const reactjsHeading = React.createElement(
-  "h1",
-  { id: "jumper" },
-  "Hello Javascript"
-);
-const reactjsRoot = ReactDOM.createRoot(
-  document.getElementById("react-js-root")
-);
-reactjsRoot.render(reactjsHeading);
+import React from "react";
+import ReactDOM from "react-dom/client";
