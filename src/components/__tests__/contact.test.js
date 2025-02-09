7eb81e5b81891f3e2386b9a8e6c1777a23ef4b2e
Mohit Kumar <mohitkumarxm@gmail.com>
2025-02-10 00:30:27 +0530

    test cases added and run

--- src/components/__tests__/contact.test.js
+++ src/components/__tests__/contact.test.js
@@ -1,9 +1,38 @@
-import { render, screen } from "@testing-library/dom";
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
 import Contact from "../contact";
 
 test("Test Contact Render", () => {
   render(<Contact />);
 
-  const heading = screen.getByText("Contact Us");
+  const button = screen.getByRole("button");
+
+  // Assertion
+  expect(button).toBeInTheDocument();
+});
+
+test("Test Contact Render", () => {
+  render(<Contact />);
+
+  const heading = screen.getByRole("heading");
+
+  // Assertion
+  expect(heading).toBeInTheDocument();
+});
+test("Test Contact Render", () => {
+  render(<Contact />);
+
+  const textbox = screen.getAllByRole("textbox");
+  console.log(textbox);
+
+  // Assertion
+});
+
+test("Test Contact Render", () => {
+  render(<Contact />);
+
+  const heading = screen.getByText("Contact Us Page");
+
+  // Assertion
   expect(heading).toBeInTheDocument();
 });
